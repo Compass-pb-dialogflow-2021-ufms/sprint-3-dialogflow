df539c1b4237b3dd34eecefd5c2d1d34b24e9385
arthcabral <ac-silva95@hotmail.com>
2021-12-14 11:47:51 -0400

    Mudancas finais

--- README.md
+++ README.md
@@ -1,42 +1,44 @@
-# Avaliação Sprint 3 - Programa de Bolsas Compass.uol e UFMS
+# Chatbot Tre Fratelli
 
-Terceira sprint do programa de bolsas Compass.uol para formação em chatbot Dialogflow.
+## On the road
 
-
-## Execução
-
-- Criar Chatbot em Dialogflow para atender esta necessidade:
-  - A Pizzaria Tre Fratelli te contratou para fazer um bot para atender seus clientes.
-O bot deve conseguir mostrar ao usuário quais os sabores disponíveis, e guiá-lo ao longo de um pedido.
-Para fazer o pedido, são necessárias três informações:
-    - qual o sabor da pizza;
-    - qual o tamanho;
-    - qual a forma de pagamento. 
-
-  - Menu:
-    - 5 pizzas salgadas:
-      - 4 queijos, camarão, calabresa, atum, frango;
-    - 3 pizzas doces:
-      - brigadeiro, banana, doce de leite;
-    - 4 tipos de bebida:
-      - refrigerante, suco, água, água de coco;
-    - 4 tamanhos de pizza:
-      - broto, média, grande, família; 
-    - Opção de pizzas com ou sem borda recheada.
-    
-- Ao final, deve mostrar as opções escolhidas, o valor do pedido, e perguntar se o cliente deseja concluir o pedido.
-
-- Canal: Telegram.
-
-- Subir serviço Nodejs que trate as respostas;
+  Estou bem insatisfeito com o que eu fiz. Essa foi a avaliação que eu mais tive dificuldade, por uma série de motivos. Primeira vez implementando algo do gênero, gastei muito tempo pensando em alternativas, estimei mal o tempo para a realização da avaliação, visto que nunca tinha feito algo desse tamanho antes. Também tentei modular o código, entretanto quebrou o código e já não existia tempo para solucionar os erros. Não quero de forma nenhuma justificar o meu desempenho, desejo apenas informar o aconteceu. 
+  Diante disso, irei levar essa experiência como aprendizado para as próximas atividades e buscar melhorar meu pontos fracos que essa atividade revelou. 
 
 
-## Entrega
+## Sobre o bot
+  A aplicação dá boas vindas para o usuário e também exibe os produtos com os preços. O usuário pode escolher o que deseja entre pizza e bebida. 
+  
+<ul>
+  <li>Express - para criar o servidor e e criar as rotas.</li>
+  <li>actions-on-google - para auxiliar no tratamento das respostas</li>
+  <li>dialogflow-fulfillment - realizar a análise do corpo das solicitações.</li>
+  
+</ul>
 
-- Aceitar o convite do repositório da sprint-3-dialogflow;
+## Execução  
 
-- Criar uma branch no repositório com o formato nome-sobrenome;
+  Primeiramente é necessário que o Node.JS e o Ngrok esteja instalado na máquina. 
+  
+  Clone este repositório e no mesmo diretório instale as depenêcias pelo terminal:
 
-- Subir o trabalho na branch com um readme.md, documentando detalhes sobre como a avaliação foi desenvolvida e como utilizar o sistema.
+  ```node
+  npm i express dialogflow-fulfillment actions-on-google
+  ```
+  Após a instalação dos pacotes, ainda no terminal, digite
+  ```
+  npm start
+  ```
+  O terminal vai informar que a aplicação está funcionando na porta 3000, basta acessar no seu navagador preferido: 
+```
+localhost:3003
+  ```
+  Após ter feito o passo anterior, abra o terminal na pasta onde se encontra o ngrok e digite
+```
+./ngrok http 3003
+  ```
+ Copie o primeiro endereço "forwarding"gerado no webhook dentro do fulfillmet do bot no DialogFlow.
+  
+O Bot também está no telegram: https://t.me/teste1_bot ou busque dentro do telegram pelo @teste1_bot
 
-- O prazo de entrega é até às 13h do dia 14/12 no repositório do github (https://github.com/Compass-pb-dialogflow-2021-ufms/sprint-3-dialogflow).
+![Example](bot.png)
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fulfill = require("dialogflow-fulfillment");
-const preco = require('./Itens/preco');
+const preco = require("./Itens/preco");
 
 const app = express();
 
@@ -14,39 +14,39 @@ app.post("/", express.json(), (req, res) => {
     response: res,
   });
 
-  function cardapio(){
+  function cardapio() {
     agent.add(` Pizzas tradicionais:
         4 Queijos: R$${preco.sabor.quatroQuiejos} |
         Atum: R$${preco.sabor.atum} |
         Calabresa: R$${preco.sabor.calabresa} |
         Camarão: R$${preco.sabor.camarao} |
-        Frango: R$${preco.sabor.frango}`); 
+        Frango: R$${preco.sabor.frango}`);
     agent.add(` Pizzas doces: 
         Banana: R$${preco.sabor.banana} |
         Brigadeiro: R$${preco.sabor.brigadeiro} |
         Doce de Leite: R$${preco.sabor.doceDeLeite} `);
   }
 
-  function cardapioBebidas(){
+  function cardapioBebidas() {
     agent.add(` Bebidas:
       Água: R$${preco.bebida.agua}
       Água de Coco: R$${preco.bebida.aguaCoco}
       Refrigerante: R$${preco.bebida.refrigerante}
       Suco: R$${preco.bebida.suco}
-    `)
+    `);
   }
-  
+
   //primeiro contato com o cliente
   function welcome(agent) {
-    agent.add('Olá, seja bem vindo(a) a Pizzaria Tre Fratelli.');
+    agent.add("Olá, seja bem vindo(a) a Pizzaria Tre Fratelli.");
 
-    agent.add('Este é o cardápio de pizzas da casa: ');
+    agent.add("Este é o cardápio de pizzas da casa: ");
     cardapio();
-    
-    agent.add('Essas são as bebidas disponíveis: ');
+
+    agent.add("Essas são as bebidas disponíveis: ");
     cardapioBebidas();
 
-    agent.add('Gostaria de pedir uma pizza ou alguma bebida?')
+    agent.add("Gostaria de pedir uma pizza ou alguma bebida?");
   }
   //resposta caso o bot não compreenda a mensagem
   function fallback(agent) {
@@ -54,32 +54,46 @@ app.post("/", express.json(), (req, res) => {
   }
   //pedido de pizza
   function pedirPizza(agent) {
-  
-
     let quantidade = agent.parameters.quantidade;
     let sabor = agent.parameters.sabor;
-    let tamanho = agent.parameters.tamanho; 
+    let tamanho = agent.parameters.tamanho;
     let borda = agent.parameters.borda;
 
     agent.add(
       `Certo, ${quantidade} pizza(s) de sabor ${sabor} e tamanho ${tamanho} e ${borda}! Alguma bebida para acompanhar?`
     );
   }
+  //Tentei manipular o contexto pelo nodejs com a biblioteca do dialogflow fulfillment, porém não obtive sucesso (me custou mt tempo isso )
+  // function pedirPizzaNop(agent) {
+  //   //pedidoContexto = agent.context.get('pedirPizza')
+  //   let quantidade = agent.parameters.get('pedirPizza');
 
-  function pedirPizzaNop(agent) {
-    //pedidoContexto = agent.context.get('pedirPizza')
-    let quantidade = agent.parameters.get('pedirPizza');
-    
-    agent.add(`Ok, o pedido ficou assim: ${quantidade}`);
+  //   agent.add(`Ok, o pedido ficou assim: ${quantidade}`);
 
+  // }
+
+  function pedirPizzaYes(agent) {
+    agent.add("Gostaria de qual bebida?");
+    cardapioBebidas();
   }
+
+  function pedirBebida(agent) {
+    let quantidade = agent.parameters.quantidade;
+    let bebida = agent.parameters.tiposbebida;
+    cardapioBebidas();
+    
+  }
+
+  function confirmaBebida(agent) {}
 
   let intentMap = new Map();
 
   intentMap.set("Default Welcome Intent", welcome);
   intentMap.set("Default Fallback Intent", fallback);
   intentMap.set("pedirPizza", pedirPizza);
-  intentMap.set("pedirPizza - no", pedirPizzaNop);
+  intentMap.set("pedirBebida", pedirBebida);
+  intentMap.set("pedirPizza - yes", pedirPizzaYes);
+  //intentMap.set("pedirPizza - no", pedirPizzaNop);
 
   agent.handleRequest(intentMap);
 });
